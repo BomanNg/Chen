09e770226e3df4c3b9b051a2bea4f9e98d516c9b
Boman Ng <boman.ng@qq.com>
2023-11-10 05:32:47 +0000

    refact: Build basic workflow for run lab

--- lab/run.py
+++ lab/run.py
@@ -1,5 +1,10 @@
+import logging
 import os
 from pathlib import Path
+from typing import List
+
+from lab import config
+from modules import LLM, Cores, Dataset, Embedding
 
 # Import private lab env
 labEnv = Path('.lab.env', 'r')
@@ -7,3 +12,29 @@ with open(labEnv) as file:
     for line in file:
         key, value = line.strip().split('=', 1)
         os.environ[key] = value
+
+
+def apply():
+    ...
+
+
+def metric():
+    ...
+
+
+def runLab(scen: List[str], dataset):
+    llm = LLM()
+    embedding = Embedding()
+
+    # build clsCore
+    clsCore = Cores(scen, llm, embedding)
+
+    # build test set
+    testSet = Dataset(dataset)
+
+    # run
+    apply(clsCore, testSet, embedding)
+
+    # metric
+    logPath = Path()
+    metric(logPath)
