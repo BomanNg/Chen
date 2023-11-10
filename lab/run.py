655e50c6898d323ac5259a492a6b29afceb1636f
Boman Ng <boman.ng@qq.com>
2023-11-10 06:10:49 +0000

    feat: add logger

--- lab/run.py
+++ lab/run.py
@@ -6,12 +6,20 @@ from typing import List
 from lab import config
 from modules import LLM, Cores, Dataset, Embedding
 
+labLogger = logging.getLogger("LabLogger")
+dataLogger = logging.getLogger("DataLogger")
+
 # Import private lab env
-labEnv = Path('.lab.env', 'r')
-with open(labEnv) as file:
-    for line in file:
-        key, value = line.strip().split('=', 1)
-        os.environ[key] = value
+localLabEnv = ".lab.env"
+try:
+    labLogger.info(f"Importing lab environment from {localLabEnv}")
+    labEnv = Path(localLabEnv, "r")
+    with open(labEnv) as file:
+        for line in file:
+            key, value = line.strip().split("=", 1)
+            os.environ[key] = value
+except FileNotFoundError as e:
+    labLogger.warning(f"Failed to load PATH. File '{localLabEnv}' not found.")
 
 
 def apply():
